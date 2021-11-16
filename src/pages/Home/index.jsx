af83d56cadc69b71358d2a4b9c39e5635eaa5477
Victor Alves <victorhugo_sp4@hotmail.com>
2021-11-16 16:38:49 -0300

    inicio pagina home com styled component

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./style";
+
+const Home = () => {
+    return(
+        <Container>
+            texto muito incrivel
+        </Container>
+    );
+}
+
+export default Home;
