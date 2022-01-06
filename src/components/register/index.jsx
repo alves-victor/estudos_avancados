a67a17abfbf6d10a63615de7d26d9bb3dd38d990
Victor Alves <victorhugo_sp4@hotmail.com>
2022-01-06 18:06:34 -0300

    adição de componente register

---
+++ src/components/register/index.jsx
@@ -0,0 +1,16 @@
+import { RegisterBG, Title, } from "./style";
+
+const Register = () => {
+    return(
+        <RegisterBG>
+            <Title>
+                Registro
+            </Title>
+
+            <input placeholder="Registrar Pessoa"/>
+
+        </RegisterBG>
+    );
+}
+
+export default Register;
---
+++ src/components/register/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const RegisterBG = styled.div`
+    width: 50vw;
+    height: 80vh;
+    margin: auto;
+    background-color: #AAB0B5;
+    text-align: center;
+    border-radius: 10px;
+    box-shadow: 0px 0px 6px;
+`;
+
+export const Title = styled.div`
+    font-size: 30px;
+    height: 15%;
+    line-height: 70px;
+`;
