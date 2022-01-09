74c0b9fa533ce7c0a2c6b08be2bafd0d704ce437
Victor Alves <victorhugo_sp4@hotmail.com>
2022-01-09 18:23:57 -0300

    começo da lógica de negócio

--- src/components/register/index.jsx
+++ src/components/register/index.jsx
@@ -1,13 +1,31 @@
+import { useState } from "react";
 import { RegisterBG, Title, } from "./style";
 
 const Register = () => {
+
+    const [register, setRegister] = useState("");
+
+    function handleKeyPress(e){
+        if(e.key === 'Enter'){
+            const post = async () => {
+                const user = await fetch("url")
+                .then(res => res.json())
+                .catch(e => {if(e){ alert(`Erro ${e}`); }});
+            }
+        }
+    }
+
     return(
         <RegisterBG>
             <Title>
                 Registro
             </Title>
 
-            <input placeholder="Registrar Pessoa"/>
+            <input 
+                placeholder="Registrar Pessoa"
+                onChange={(event) => setRegister(event.target.value)}
+                onKeyPress={handleKeyPress}
+            />
 
         </RegisterBG>
     );
