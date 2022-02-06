0c6c56be359799b350ddd4bf0575861bfa473fd9
Victor Alves <victorhugo_sp4@hotmail.com>
2022-02-06 19:26:37 -0300

    estilização do input de texto e display grid na div main

--- src/components/register/style.js
+++ src/components/register/style.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
-export const RegisterBG = styled.div`
+export const Main = styled.div`
+    display: grid;
     width: 50vw;
     height: 80vh;
     margin: auto;
@@ -12,6 +13,13 @@ export const RegisterBG = styled.div`
 
 export const Title = styled.div`
     font-size: 30px;
-    height: 15%;
-    line-height: 70px;
+    margin: auto;
+`;
+
+export const Submit = styled.input`
+    width: 50%;
+    height: 20%;
+    font-size: 20px;
+    margin: 0 auto;
+
 `;
